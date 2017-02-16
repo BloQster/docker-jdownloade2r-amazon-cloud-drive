3b9baa1e72ad32dad479d5d1232656f03f7640a2
Philipp Holler <philipp.holler93@googlemail.com>
2017-02-16 23:53:40 +0100

    Update jdownloader2-acd-filebot_entrypoint.sh
    changed env var names due to change in rclone

--- jdownloader2-acd-filebot_entrypoint.sh
+++ jdownloader2-acd-filebot_entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 export RCLONE_CONFIG_FILE="$JDOWNLOADER2_INSTALLDIR"/.rclone.conf
 
 if [ ! -f "$RCLONE_CONFIG_FILE" ]; then
-	if [ -n "$RCLONE_CONFIG" ]; then
-		echo "$RCLONE_CONFIG" > "$RCLONE_CONFIG_FILE"
+	if [ -n "$RCLONE_CONFIG_FILE_CONTENTS" ]; then
+		echo "$RCLONE_CONFIG_FILE_CONTENTS" > "$RCLONE_CONFIG_FILE"
 	else
 		echo "rclone config file does not exist and is not present in the environment variables."
 	fi
